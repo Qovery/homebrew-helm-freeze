1e5054bcb9450ee9e4346302737c9c6958cf0113
Pierre Mavro <pmavro@qovery.com>
2020-08-31 01:50:36 +0200

    Brew formula update for helm-freeze version v0.2

--- Formula/helm-freeze.rb
+++ Formula/helm-freeze.rb
@@ -2,16 +2,16 @@
 class HelmFreeze < Formula
   desc "Freeze your charts in the wished versions"
   homepage ""
-  version "0.1.1"
+  version "0.2"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/Qovery/helm-freeze/releases/download/v0.1.1/helm-freeze_0.1.1_darwin_amd64.tar.gz"
-    sha256 "6dd86c1048ec543e0f67de58f8eb2c95ef505ff0f08e68f44e7a718d2741129c"
+    url "https://github.com/Qovery/helm-freeze/releases/download/v0.2/helm-freeze_0.2_darwin_amd64.tar.gz"
+    sha256 "6debb3339506dfa970d27643cb14fedae7c017f4418b2886c5f3212b7006935c"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/Qovery/helm-freeze/releases/download/v0.1.1/helm-freeze_0.1.1_linux_amd64.tar.gz"
-      sha256 "e03db31b66fbd40790af796bf43a9b922e06b907fdace23408cf755ed0c69320"
+      url "https://github.com/Qovery/helm-freeze/releases/download/v0.2/helm-freeze_0.2_linux_amd64.tar.gz"
+      sha256 "5e8261d17200062952f832c137307acee088d381e29a32d52da745a8fba19920"
     end
   end
 
